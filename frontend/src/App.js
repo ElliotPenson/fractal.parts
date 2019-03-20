b0e4443811856790539813e486d133f5aa868197
Elliot Penson <elliot.penson@it.com>
2019-03-20 19:18:27 -0400

    Bootstrap the frontend with create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            This is fractal.parts.
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
