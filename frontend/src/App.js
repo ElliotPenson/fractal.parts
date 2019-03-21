807ed860b218bbe2938cde65f9f0b6e44aceb952
Elliot Penson <elliot.penson@it.com>
2019-03-20 23:29:31 -0400

    Include Ant Design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, Row, Col } from 'antd/lib';
 
 import './App.css';
 
@@ -8,17 +9,19 @@ class App extends Component {
         return (
             <div className="App">
               <header className="App-header">
-                <p>This is fractal.parts.</p>
-                 <Link to="/create">
-                   <button type="button">
-                     Create
-                   </button>
-                 </Link>
-                <Link to="/browse">
-                   <button type="button">
-                     Browse
-                   </button>
-                 </Link>
+                <h2>This is fractal.parts.</h2>
+                <Row>
+                  <Col span={12}>
+                    <Link to="/create">
+                      <Button type="primary">Create</Button>
+                    </Link>
+                  </Col>
+                  <Col span={12}>
+                    <Link to="/browse">
+                      <Button>Browse</Button>
+                    </Link>
+                  </Col>
+                </Row>
               </header>
             </div>
         );
