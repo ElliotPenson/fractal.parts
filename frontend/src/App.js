06c90e3887b65dddbc31469d6f4d946494231848
Elliot Penson <elliot.penson@it.com>
2019-03-21 18:25:16 -0400

    Reduce bundle size with babel-plugin-import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Row, Col } from 'antd/lib';
+import { Button, Row, Col } from 'antd';
 
 import './App.css';
 
