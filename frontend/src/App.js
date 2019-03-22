ec3d9203dfb0f2364adf2f5d9c2a446b6035d26c
Elliot Penson <elliot.penson@it.com>
2019-03-21 23:45:07 -0400

    Move to a white homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Row, Col } from 'antd';
+import { Button, Row, Col, Typography } from 'antd';
 
 import './App.css';
+
+const { Title } = Typography;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h2>This is fractal.parts.</h2>
+          <Title>fractal.parts</Title>
+          <p>Design and view beautiful fractals.</p>
           <Row>
             <Col span={12}>
-              <Link to="/create">
-                <Button type="primary">Create</Button>
+              <Link to="/browse">
+                <Button size="large">Browse</Button>
               </Link>
             </Col>
             <Col span={12}>
-              <Link to="/browse">
-                <Button>Browse</Button>
+              <Link to="/create">
+                <Button size="large">Create</Button>
               </Link>
             </Col>
           </Row>
