4dd06c4630dfedf70d240173eb1e6b3e79d5d28f
Elliot Penson <elliot.penson@it.com>
2019-03-21 23:49:18 -0400

    Add a canvas to the 'create' page

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react';
+import { Typography, Button } from 'antd';
+
+import Canvas from './Canvas';
+import './Create.css';
+
+const { Title } = Typography;
 
 class Create extends Component {
   constructor(props) {
     super(props);
-    this.canvasRef = React.createRef();
+    this.state = {
+      title: 'Enter a title here.',
+      context: null
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  componentDidMount() {
-    const canvas = this.canvasRef.current;
-    const context = canvas.getContext('2d');
-    context.fillRect(100, 100, 200, 200);
+  componentDidUpdate() {
+    const { context } = this.state;
+    context.globalAlpha = 0.8;
+    context.fillStyle = '#096dd9';
+    context.fillRect(100, 100, 250, 250);
+  }
+
+  handleChange(title) {
+    console.log(title);
+    this.setState({ title });
   }
 
   render() {
     return (
       <div className="Create">
-        <p>Create fractal</p>
-        <canvas ref={this.canvasRef} />
+        <Title editable={{ onChange: this.handleChange }}>
+          {this.state.title}
+        </Title>
+        <Canvas
+          width="750"
+          height="750"
+          onContext={context => this.setState({ context })}
+        />
+        <div>
+          <Button type="primary" size="large">
+            New
+          </Button>
+          <Button size="large">Preview</Button>
+          <Button size="large">Publish</Button>
+          <Button size="large">Help</Button>
+        </div>
       </div>
     );
   }
