eab6ab6e4c56ada0952979f2726a8c8b870b66a0
Elliot Penson <elliot.penson@it.com>
2019-05-11 22:54:16 -0400

    Write a knex migration script for table creation

---
+++ services/migrations/20190509213147_create_fractals_table.js
@@ -0,0 +1,18 @@
+exports.up = knex => {
+  return knex.schema.createTable('fractals', table => {
+    table.increments();
+    table.string('key');
+    table.string('title');
+    table.text('body');
+    table.integer('views');
+    table.timestamp('created_at');
+    table.unique('key');
+    table.index('key');
+    table.index('views');
+    table.index('created_at');
+  });
+};
+
+exports.down = knex => {
+  return knex.schema.dropTable('fractals');
+};
