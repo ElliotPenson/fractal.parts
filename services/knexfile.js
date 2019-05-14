91f7e8db7e599572d00ee9be5114786f4609198a
Elliot Penson <elliot.penson@it.com>
2019-05-13 23:12:15 -0400

    Add mysql development knexfile

---
+++ services/knexfile.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: {
+      host: '127.0.0.1',
+      database: 'fractaldb',
+      user: 'user',
+      password: 'password'
+    }
+  }
+};
