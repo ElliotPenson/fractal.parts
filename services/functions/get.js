73204c4fd564fecadabb3e28fcd450e61129a24f
Elliot Penson <elliot.penson@it.com>
2019-05-15 23:00:33 -0400

    Check a fractal exists before trying to increment its view count

--- services/functions/get.js
+++ services/functions/get.js
@@ -1,15 +1,15 @@
-const { get, increment } = require('./database');
+const { exists, get, increment } = require('./database');
 const { buildResponse, HttpStatus } = require('./utilities');
 
 exports.handle = async (event, context, other) => {
   const { key } = event.pathParameters;
-  await increment(key);
-  const fractal = await get(key);
-  if (!fractal) {
+  if (!(await exists(key))) {
     return buildResponse(
       { message: `Fractal with key '${key}' does not exist` },
       HttpStatus.NOT_FOUND
     );
   }
+  await increment(key);
+  const fractal = await get(key);
   return buildResponse(fractal);
 };
