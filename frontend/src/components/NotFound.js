ec15945e898f7d94f58233ec95e7831387084884
Elliot Penson <elliot.penson@it.com>
2019-05-26 20:30:06 -0400

    Implement a page for 404 Not Found

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import { Typography, Icon } from 'antd';
+
+import './NotFound.css';
+
+const { Title } = Typography;
 
 class NotFound extends Component {
   render() {
     return (
       <div className="NotFound">
-        <p>Not found!</p>
+        <Icon type="file-exclamation" />
+        <Title>Not Found.</Title>
       </div>
     );
   }
--- frontend/src/components/View.js
+++ frontend/src/components/View.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Typography } from 'antd';
+import { Typography, Icon } from 'antd';
 
+import NotFound from './NotFound';
 import Attractor from './Attractor';
 import { get } from '../api';
 
@@ -11,36 +12,40 @@ const { Title } = Typography;
 class View extends Component {
   constructor() {
     super();
-    this.state = { fractal: null };
+    this.state = { fractal: null, status: null };
   }
 
   async componentDidMount() {
     const { pathname } = this.props.location;
     const key = pathname.slice(1);
     try {
-      const { data } = await get(key);
-      this.setState({ fractal: data });
+      const { data, status } = await get(key);
+      this.setState({ fractal: data, status });
     } catch (error) {
-      console.error(error);
+      const { status } = error.response;
+      this.setState({ status });
     }
   }
 
   render() {
-    const { fractal } = this.state;
-    if (!fractal) {
+    const { fractal, status } = this.state;
+    if (status === 404) {
+      return <NotFound />;
+    } else if (!fractal) {
       return null;
+    } else {
+      return (
+        <div className="View">
+          <Title>{fractal.title}</Title>
+          <p>with {fractal.views} views</p>
+          <Attractor
+            width={window.innerWidth}
+            height={window.innerHeight}
+            fractal={fractal.body}
+          />
+        </div>
+      );
     }
-    return (
-      <div className="View">
-        <Title>{fractal.title}</Title>
-        <p>with {fractal.views} views</p>
-        <Attractor
-          width={window.innerWidth}
-          height={window.innerHeight}
-          fractal={fractal.body}
-        />
-      </div>
-    );
   }
 }
 
